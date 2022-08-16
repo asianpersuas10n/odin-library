100445df4c5c4854b23ff112e3b026b825c4402c
nick <nicholasjpenaranda@gmail.com>
2022-08-15 23:47:51 -0500

    added form vailidation and airbnb style

--- constructorJS.js
+++ constructorJS.js
@@ -1,6 +1,10 @@
-let myLibrary = [];
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+const myLibrary = [];
 
-//dom calls
+// dom calls
 
 const bookGrid = document.querySelector(".bookGrid");
 const newBookBtn = document.querySelector(".newBook");
@@ -8,84 +12,85 @@ const form = document.querySelector("form");
 const inputTitle = document.querySelector("#title");
 const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#pages");
-const add = document.querySelector(".add")
 
-//dom minip
+// dom minip
 
-newBookBtn.addEventListener("click", function() {
-    form.style.display = "block";
+newBookBtn.addEventListener("click", () => {
+  form.style.display = "block";
 });
-add.addEventListener("click", function() {
-    form.style.display = "none";
-})
 
-
-class Book{
-    constructor(title, author, pages, haveRead) {
-        this.title = title
-        this.author = author
-        this.pages = pages
-        this.haveRead = haveRead
-    }
-};
-
+class Book {
+  constructor(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+  }
+}
 
 // takes user input and stores the obj into arr
 
 function addBookToLibrary(title, author, pages, haveRead) {
-    let newBook = new Book(title, author, pages, haveRead)
-    myLibrary.push(newBook);
-    updateHtml();
+  const newBook = new Book(title, author, pages, haveRead);
+  myLibrary.push(newBook);
+  updateHtml();
 }
 
 function updateHtml() {
-    while(bookGrid.childNodes.length >  0) {
-        bookGrid.removeChild(bookGrid.lastChild);
-      };
-    myLibrary.forEach(x => {
-        libraryToHtml(x);
-    });
-};
+  while (bookGrid.childNodes.length > 0) {
+    bookGrid.removeChild(bookGrid.lastChild);
+  }
+  myLibrary.forEach((x) => {
+    libraryToHtml(x);
+  });
+}
 
 function libraryToHtml(book) {
-        const div = document.createElement("div");
-        const p = document.createElement("p");
+  const div = document.createElement("div");
+  const p = document.createElement("p");
 
-        const readButton = document.createElement("button");
-        readButton.textContent = "Change Read Status"
-        readButton.classList.add("readButton");
+  const readButton = document.createElement("button");
+  readButton.textContent = "Change Read Status";
+  readButton.classList.add("readButton");
 
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Remove";
-        deleteBtn.classList.add("remove");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Remove";
+  deleteBtn.classList.add("remove");
 
-        p.innerText = `Title: ${book.title} \n Author: ${book.author} \n Number of Pages: ${book.pages} \n Have Read? ${book.haveRead}`;
+  p.innerText = `Title: ${book.title} \n Author: ${book.author} \n Number of Pages: ${book.pages} \n Have Read? ${book.haveRead}`;
 
-        div.appendChild(p);
-        div.appendChild(readButton);
-        div.appendChild(deleteBtn);
-        div.class = "gridItem";
-        div.classList.add("gridItem");
-        bookGrid.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(readButton);
+  div.appendChild(deleteBtn);
+  div.class = "gridItem";
+  div.classList.add("gridItem");
+  bookGrid.appendChild(div);
 
-        readButton.addEventListener("click", () => {
-            if(book.haveRead === "No") {
-                book.haveRead = "Yes";
-            } else {
-                book.haveRead = "No";
-            };
-            updateHtml();
-        });
+  readButton.addEventListener("click", () => {
+    if (book.haveRead === "No") {
+      book.haveRead = "Yes";
+    } else {
+      book.haveRead = "No";
+    }
+    updateHtml();
+  });
 
-        deleteBtn.addEventListener('click', () => {
-            myLibrary.splice(myLibrary.indexOf(book), 1);
-            deleteBtn.parentElement.remove();
-            updateHtml();
-          });
-};
+  deleteBtn.addEventListener("click", () => {
+    myLibrary.splice(myLibrary.indexOf(book), 1);
+    deleteBtn.parentElement.remove();
+    updateHtml();
+  });
+}
 
 function formValues() {
-    const inputHaveRead = document.querySelector('input[name="haveRead"]:checked');
-    addBookToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, inputHaveRead.value);
-};
-
+  const inputHaveRead = document.querySelector(
+    'input[name="haveRead"]:checked'
+  );
+  addBookToLibrary(
+    inputTitle.value,
+    inputAuthor.value,
+    inputPages.value,
+    inputHaveRead.value
+  );
+  form.style.display = "none";
+}
